dc46c9ce5477bd66b915cbb0921b32b65fb496b1
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-01-07 21:43:34 +0400

    finished home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           alt="MoustacheMan"
           width={560}
           height={560}
-          className="w-[300px] h-[300px] z-[1] md:h-[560px] md:w[560px]/="
+          className="w-[300px] h-[300px] z-[1] md:h-[560px] md:w-[560px]"
         />
       </div>
       <Image
@@ -19,6 +19,27 @@ export default function Home() {
         height={230}
         className="absolute left-0 top-0"
       />
+
+      <div className="flex flex-col gap-3 z-[10] pl-40 pt-20">
+        <h1 className="text-[50px] text-white max-w-[500px]">
+          Transforming ideas into{" "}
+          <span className="text-red-500">Digital Reality</span>
+        </h1>
+        <p className="text-[16px] tegt-gray-200 md:text-gray-400 mb-10 md:pb-2 max-w-[400px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+          voluptatum quia itaque impedit quos quis ab tenetur, aperiam hic
+          dolore.
+        </p>
+        <div className="relative w-[100p] h0-[100px] md:w[160px] md:h[160px] md:h[160px] mt-5">
+          <Image
+            src="/assets/rounded-text.png"
+            alt="projects"
+            width={160}
+            height={160}
+            className="slow-spin"
+          />
+        </div>
+      </div>
     </main>
   );
 }
