7a72a68263d1abe12ea38f163a8140df06e28f9f
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-01-10 21:14:26 +0400

    added sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <Sidebar />
         {children}
       </body>
     </html>
