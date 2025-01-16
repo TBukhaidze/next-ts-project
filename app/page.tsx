e345b07a43d2d9adfce053737a3949e98d3fc9e9
Temo Bukhaidze <bukhaidze.temo@gmail.com>
2025-01-16 18:53:58 +0400

    improved responsiveness for home page and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex items-center h-screen relative bg-cover bg-[url('/assets/bg-explosion.png')]">
+    <main className="flex flex-col sm:flex-row items-center h-screen relative bg-cover bg-[url('/assets/bg-explosion.png')]">
       <div className="absolute right-10 md:right-40 bottom-0 z-[10]">
         <Image
           src="/assets/MoustacheMan.png"
@@ -20,17 +20,17 @@ export default function Home() {
         className="absolute left-0 top-0"
       />
 
-      <div className="flex flex-col gap-3 z-[10] pl-40 pt-20">
-        <h1 className="text-[50px] text-white max-w-[500px]">
+      <div className="flex flex-col gap-3 z-[10] pl-6 sm:pl-40 pt-20">
+        <h1 className="text-[35px] sm:text-[50px] text-white sm:max-w-[500px] max-w-[300px] mt-20">
           Transforming ideas into{" "}
           <span className="text-red-500">Digital Reality</span>
         </h1>
-        <p className="text-[16px] tegt-gray-200 md:text-gray-400 mb-10 md:pb-2 max-w-[400px]">
+        <p className="text-[14px] sm:text-[16px] text-gray-200 sm:text-gray-400 mb-5 sm:mb-10 sm:pb-2 max-w-[400px]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
           voluptatum quia itaque impedit quos quis ab tenetur, aperiam hic
           dolore.
         </p>
-        <div className="relative w-[100p] h0-[100px] md:w[160px] md:h[160px] md:h[160px] mt-5">
+        <div className="relative w-[100px] h-[100px] sm:w-[160px] sm:h-[160px] mt-5">
           <Image
             src="/assets/rounded-text.png"
             alt="projects"
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 bg-transparent z-[20] w-full flex  gap-5 md:justify-between md:px-60 p-5">
-      <h1 className="text-white text-[45px]">
+    <div className="fixed top-0 bg-transparent z-[20] w-full flex flex-col md:flex-row gap-5 md:justify-between md:px-60 p-5">
+      <h1 className="text-white text-[35px] sm:text-[45px]">
         Temuri <span className="font-thin">Bukhaidze</span>
         <span className="text-red-500">.</span>
       </h1>
-      <div className="flex flex-row gap-5">
+      <div className="flex flex-row items-center justify-center gap-5">
         {Socials.map((social) => (
           <Image
             key={social.name}
